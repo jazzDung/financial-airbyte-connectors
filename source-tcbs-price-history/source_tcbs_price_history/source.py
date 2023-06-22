92d9f50a4ea6b5a52d9eddf9c3e9ad0276c78e37
jazzDung <dungpham.020901@gmail.com>
2023-06-23 01:36:39 +0700

    INCREMENTAL SYNC BABYYY

--- source-tcbs-price-history/source_tcbs_price_history/source.py
+++ source-tcbs-price-history/source_tcbs_price_history/source.py
@@ -14,19 +14,32 @@ from airbyte_cdk.sources.streams import Stream, IncrementalMixin
 from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator, NoAuth
 
-class Symbol(HttpStream):
+class Symbol(HttpStream, IncrementalMixin):
     url_base = None
     primary_key = 'tradingDate'
+    cursor_field = "tradingDate"
 
     def use_cache(self) -> bool:
         return True
 
-    def __init__(self, config: Mapping[str, Any], **kwargs):
+    def __init__(self, config: Mapping[str, Any], start_date: datetime, **kwargs):
         super().__init__()
 
-        self.sync_all = config['sync_all']
-        self.days_before = config['days_before']
+        self.fast_mode = config['fast_mode']
         self.url = config["symbol_url"]
+        # self.start_date = start_date
+        self._cursor_value = None
+
+    @property
+    def state(self) -> Mapping[str, Any]:
+        if self._cursor_value:
+            return {self.cursor_field: self._cursor_value.strftime('%Y-%m-%d')}
+        else:
+            return {self.cursor_field: self.start_date.strftime('%Y-%m-%d')}
+    
+    @state.setter
+    def state(self, value: Mapping[str, Any]):
+       self._cursor_value = datetime.strptime(value[self.cursor_field][:10], '%Y-%m-%d')
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         return None
@@ -49,19 +62,24 @@ class Symbol(HttpStream):
         
         response = response.text.split(",")
         
-        if self.sync_all:
-            return response
-        else:
+        if self.fast_mode:
             return response[:10]
+        else:
+            return response
+
 
 class SymbolSubStream(HttpSubStream, Symbol, ABC):
     raise_on_http_errors = False
+    primary_key = 'tradingDate'
+    cursor_field = "tradingDate"
  
     def __init__(self, config: Mapping[str, Any], parent: Symbol, **kwargs):
         super().__init__(config=config, parent=parent, **kwargs)
         
-        self.sync_all = config['sync_all']
+        self.sync_all_history = config['sync_all_history']
         self.days_before = config['days_before']
+        # self.start_date = self.parent.start_date
+        self._cursor_value = datetime.strptime('2000-01-01', '%Y-%m-%d')
 
         # Specify some of the timestamp here
         today = date.today()
@@ -69,18 +87,17 @@ class SymbolSubStream(HttpSubStream, Symbol, ABC):
 
         self.today = int(time.mktime(today.timetuple()))
         self.start_date = int(time.mktime(start_date.timetuple()))
- 
+    
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         yield response.json()
 
 
 class PriceHistory(SymbolSubStream):
  
-    def path(self, *, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None,
-             next_page_token: Mapping[str, Any] = None) -> str:
-        if self.sync_all:
+    def path(self, *, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> str:
+        if self.sync_all_history:
             # Sync data from day 0 (Mean, all of them)
-            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice["ticker"]}&type=stock&resolution=D&from=0&to={self.today}'
+            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice["ticker"]}&type=stock&resolution=D&from={int(time.mktime(self._cursor_value.timetuple()))}&to={self.today}'
         else:
             # Sync only the data of yesterday
             return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice["ticker"]}&type=stock&resolution=D&from={self.start_date}&to={self.today}'
@@ -88,17 +105,28 @@ class PriceHistory(SymbolSubStream):
     def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
         for stream_slices in self.parent.stream_slices(sync_mode=SyncMode.full_refresh):
             for record in self.parent.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slices):
-                # for data in record["items"]:
-                yield {"ticker": record}
+                yield {"ticker": record, "resume_date": self._cursor_value}
  
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-
         response = response.json()
         data = response['data']
         ticker = response['ticker']
         for element in data:
             element['ticker'] = ticker
             yield element
+    
+    def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
+        for record in super().read_records(*args, **kwargs):
+            if self.parent._cursor_value:
+                print("Parent: ", self.parent._cursor_value)
+                latest_record_date = datetime.strptime(record[self.cursor_field][:10], '%Y-%m-%d')
+                self._cursor_value = max(self._cursor_value, latest_record_date)
+            
+            if self._cursor_value:
+                print("Child: ", self._cursor_value)
+                latest_record_date = datetime.strptime(record[self.cursor_field][:10], '%Y-%m-%d')
+                self._cursor_value = max(self._cursor_value, latest_record_date)
+            yield record
 
 # Source
 class SourceTcbsPriceHistory(AbstractSource):
@@ -107,7 +135,21 @@ class SourceTcbsPriceHistory(AbstractSource):
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         auth = NoAuth() 
-        
+
+        if config['sync_all_history']:
+            start_date = datetime.strptime('2000-01-01', '%Y-%m-%d')
+        else:
+            today = datetime.today()
+            start_date = today - timedelta(config['days_before'])
+
         return [
-            PriceHistory(parent=Symbol(config=config, authenticator=auth), config=config, authenticator=auth),
+            PriceHistory(
+                parent=Symbol(
+                    config=config, 
+                    authenticator=auth, 
+                    start_date=start_date), 
+                config=config, 
+                authenticator=auth,
+                start_date=start_date
+            ),
         ]
