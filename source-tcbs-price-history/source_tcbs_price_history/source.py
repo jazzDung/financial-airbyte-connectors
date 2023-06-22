dff627e5da07b7d37a1b4897f7bd89b4696badd0
jazzDung <dungpham.020901@gmail.com>
2023-06-23 02:54:52 +0700

    just found new big ass bug

--- source-tcbs-price-history/source_tcbs_price_history/source.py
+++ source-tcbs-price-history/source_tcbs_price_history/source.py
@@ -22,7 +22,7 @@ class Symbol(HttpStream, IncrementalMixin):
     def use_cache(self) -> bool:
         return True
 
-    def __init__(self, config: Mapping[str, Any], start_date: datetime, **kwargs):
+    def __init__(self, config: Mapping[str, Any], cursor_start: datetime, **kwargs):
         super().__init__()
 
         self.fast_mode = config['fast_mode']
@@ -34,7 +34,7 @@ class Symbol(HttpStream, IncrementalMixin):
         if self._cursor_value:
             return {self.cursor_field: self._cursor_value.strftime('%Y-%m-%d')}
         else:
-            return {self.cursor_field: self.start_date.strftime('%Y-%m-%d')}
+            return {self.cursor_field: self.cursor_start.strftime('%Y-%m-%d')}
     
     @state.setter
     def state(self, value: Mapping[str, Any]):
@@ -71,21 +71,14 @@ class SymbolSubStream(HttpSubStream, Symbol, ABC):
     raise_on_http_errors = False
     primary_key = 'tradingDate'
     cursor_field = "tradingDate"
- 
-    def __init__(self, config: Mapping[str, Any], parent: Symbol, **kwargs):
-        super().__init__(config=config, parent=parent, **kwargs)
+     
+    def __init__(self, config: Mapping[str, Any], parent: Symbol, cursor_start:datetime, **kwargs):
+        super().__init__(config=config, parent=parent, cursor_start=cursor_start, **kwargs)
         
         self.sync_all_history = config['sync_all_history']
-        self.days_before = config['days_before']
-        self.start_date = self.parent.start_date
-        self._cursor_value = datetime.strptime('2000-01-01', '%Y-%m-%d')
-
-        # Specify some of the timestamp here
-        today = date.today()
-        start_date = today - timedelta(self.days_before)
-
-        self.today = int(time.mktime(today.timetuple()))
-        self.start_date = int(time.mktime(start_date.timetuple()))
+        self.start_date = int(time.mktime(datetime.strptime(config['start_date'], '%Y-%m-%d').timetuple()))
+        self.end_date = int(time.mktime(datetime.strptime(config['end_date'], '%Y-%m-%d').timetuple()))
+        self._cursor_value = cursor_start
     
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         yield response.json()
@@ -96,15 +89,15 @@ class PriceHistory(SymbolSubStream):
     def path(self, *, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> str:
         if self.sync_all_history:
             # Sync data from day 0 (Mean, all of them)
-            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice["ticker"]}&type=stock&resolution=D&from={int(time.mktime(self._cursor_value.timetuple()))}&to={self.today}'
+            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice["ticker"]}&type=stock&resolution=D&from={int(time.mktime(self._cursor_value.timetuple()))}&to={int(time.mktime(datetime.today().timetuple()))}'
         else:
             # Sync only the data of yesterday
-            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice["ticker"]}&type=stock&resolution=D&from={self.start_date}&to={self.today}'
+            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice["ticker"]}&type=stock&resolution=D&from={self.start_date}&to={self.end_date}'
  
     def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
         for stream_slices in self.parent.stream_slices(sync_mode=SyncMode.full_refresh):
             for record in self.parent.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slices):
-                yield {"ticker": record, "resume_date": self._cursor_value}
+                yield {"ticker": record}
  
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         response = response.json()
@@ -115,12 +108,7 @@ class PriceHistory(SymbolSubStream):
             yield element
     
     def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
-        for record in super().read_records(*args, **kwargs):
-            if self.parent._cursor_value:
-                print("Parent: ", self.parent._cursor_value)
-                latest_record_date = datetime.strptime(record[self.cursor_field][:10], '%Y-%m-%d')
-                self._cursor_value = max(self._cursor_value, latest_record_date)
-            
+        for record in super().read_records(*args, **kwargs):            
             if self._cursor_value:
                 print("Child: ", self._cursor_value)
                 latest_record_date = datetime.strptime(record[self.cursor_field][:10], '%Y-%m-%d')
@@ -130,27 +118,31 @@ class PriceHistory(SymbolSubStream):
 # Source
 class SourceTcbsPriceHistory(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
-
-        if 
+        if not config['sync_all_history'] and not config['start_date'] and not config['end_date']:
+            return False, "You must either enable sync_all_history or specify start_date and end_date"
+        elif not config['sync_all_history'] and not config['start_date']:
+            return False, "Missing end_date"
+        elif not config['sync_all_history'] and not config['end_date']:
+            return False, "Missing start_date" 
+        elif not config['sync_all_history'] and int(time.mktime(datetime.strptime(config['start_date'], '%Y-%m-%d').timetuple())) > int(time.mktime(datetime.strptime(config['end_date'], '%Y-%m-%d').timetuple())):
+            return False, "start_date is after end_date" 
+        elif not config['sync_all_history'] and config['start_date'] and config['end_date']:
+            return True, "sync_all_history are enabled and will override date range ingest mode"
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         auth = NoAuth() 
 
-        if config['sync_all_history']:
-            start_date = datetime.strptime('2000-01-01', '%Y-%m-%d')
-        else:
-            today = datetime.today()
-            start_date = today - timedelta(config['days_before'])
+        cursor_start = datetime.strptime('2000-01-01', '%Y-%m-%d')
 
         return [
             PriceHistory(
                 parent=Symbol(
                     config=config, 
                     authenticator=auth, 
-                    start_date=start_date), 
+                    cursor_start=cursor_start), 
                 config=config, 
                 authenticator=auth,
-                start_date=start_date
+                cursor_start=cursor_start
             ),
         ]
