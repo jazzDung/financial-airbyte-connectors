f0db512ec0930e69084d8edd8c7f409842b50bc2
jazzDung <dungpham.020901@gmail.com>
2023-06-27 21:02:58 +0700

    incremental is a go!

--- source-tcbs-price-history/source_tcbs_price_history/source.py
+++ source-tcbs-price-history/source_tcbs_price_history/source.py
@@ -16,7 +16,7 @@ from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator, NoAuth
 
 class Symbol(HttpStream, IncrementalMixin):
     url_base = None
-    primary_key = 'tradingDate'
+    primary_key = "tradingDate"
     cursor_field = "tradingDate"
 
     def use_cache(self) -> bool:
@@ -27,18 +27,32 @@ class Symbol(HttpStream, IncrementalMixin):
 
         self.fast_mode = config['fast_mode']
         self.url = config["symbol_url"]
-        self._cursor_value = None
+        self._cursor_value = {}
+        self.cursor_start = cursor_start
+
+        """
+        Fill the _cursor_value with ticker symbol from url
+        Format will be like this : { "TCB":946659600, "ABC":946659600}
+        """
+        
+        response = requests.get(config["symbol_url"]).text.split(",")
+
+        if self.fast_mode:
+            for symbol in response[:5]:
+                self._cursor_value[symbol] = self.cursor_start
+        else:
+            for symbol in response:
+                self._cursor_value[symbol] = self.cursor_start    
+
 
     @property
     def state(self) -> Mapping[str, Any]:
-        if self._cursor_value:
-            return {self.cursor_field: self._cursor_value.strftime('%Y-%m-%d')}
-        else:
-            return {self.cursor_field: self.cursor_start.strftime('%Y-%m-%d')}
-    
+        return self._cursor_value
+        
     @state.setter
     def state(self, value: Mapping[str, Any]):
-       self._cursor_value = datetime.strptime(value[self.cursor_field][:10], '%Y-%m-%d')
+        for key in self._cursor_value:    
+            self._cursor_value[key] = datetime.strptime(value[key][:10], '%Y-%m-%d').date()
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         return None
@@ -60,9 +74,8 @@ class Symbol(HttpStream, IncrementalMixin):
     ) -> Iterable[Mapping]:
         
         response = response.text.split(",")
-        
         if self.fast_mode:
-            return response[:10]
+            return response[:5]
         else:
             return response
 
@@ -72,14 +85,13 @@ class SymbolSubStream(HttpSubStream, Symbol, ABC):
     primary_key = 'tradingDate'
     cursor_field = "tradingDate"
      
-    def __init__(self, config: Mapping[str, Any], parent: Symbol, cursor_start:datetime, **kwargs):
+    def __init__(self, config: Mapping[str, Any], parent: Symbol, cursor_start: datetime, **kwargs):
         super().__init__(config=config, parent=parent, cursor_start=cursor_start, **kwargs)
         
         self.sync_all_history = config['sync_all_history']
-        self.start_date = int(time.mktime(datetime.strptime(config['start_date'], '%Y-%m-%d').timetuple()))
-        self.end_date = int(time.mktime(datetime.strptime(config['end_date'], '%Y-%m-%d').timetuple()))
-        self._cursor_value = cursor_start
-    
+        # self.start_date = int(time.mktime(datetime.strptime(config['start_date'], '%Y-%m-%d').timetuple()))
+        # self.end_date = int(time.mktime(datetime.strptime(config['end_date'], '%Y-%m-%d').timetuple()))
+
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         yield response.json()
 
@@ -87,34 +99,36 @@ class SymbolSubStream(HttpSubStream, Symbol, ABC):
 class PriceHistory(SymbolSubStream):
  
     def path(self, *, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> str:
+
+
         if self.sync_all_history:
             # Sync data from day 0 (Mean, all of them)
-            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice["ticker"]}&type=stock&resolution=D&from={int(time.mktime(self._cursor_value.timetuple()))}&to={int(time.mktime(datetime.today().timetuple()))}'
-        else:
-            # Sync only the data of yesterday
-            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice["ticker"]}&type=stock&resolution=D&from={self.start_date}&to={self.end_date}'
+            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice}&type=stock&resolution=D&from={int(time.mktime(self._cursor_value[stream_slice].timetuple()))}&to={int(time.mktime(datetime.today().date().timetuple()))}'
+        # else:
+        #     # Sync only the data of yesterday
+        #     return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker={stream_slice}&type=stock&resolution=D&from={self.start_date}&to={self.end_date}'
  
     def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
         for stream_slices in self.parent.stream_slices(sync_mode=SyncMode.full_refresh):
             for record in self.parent.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slices):
-                yield {"ticker": record, "single_cursor": self.sursor_start}
+                yield record
  
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         response = response.json()
-        data = response['data']
-        ticker = response['ticker']
+        data = response["data"]
+        ticker = response["ticker"]
         
         for element in data:
-            element['ticker'] = ticker
+            element["ticker"] = ticker
             yield element
     
     def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
         for record in super().read_records(*args, **kwargs):            
-            if self._cursor_value:
-                print("Child: ", self._cursor_value)
-                latest_record_date = datetime.strptime(record[self.cursor_field][:10], '%Y-%m-%d')
-                self._cursor_value = max(self._cursor_value, latest_record_date)
-            yield record
+            latest_record_date = datetime.strptime(record[self.cursor_field][:10], '%Y-%m-%d').date()
+
+            if self._cursor_value[record["ticker"]] < latest_record_date:
+                self._cursor_value[record["ticker"]] = latest_record_date
+                yield record
 
 # Source
 class SourceTcbsPriceHistory(AbstractSource):
@@ -134,7 +148,7 @@ class SourceTcbsPriceHistory(AbstractSource):
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         auth = NoAuth() 
 
-        cursor_start = datetime.strptime('2000-01-01', '%Y-%m-%d')
+        cursor_start = datetime.strptime('2000-01-01', '%Y-%m-%d').date()
 
         return [
             PriceHistory(
