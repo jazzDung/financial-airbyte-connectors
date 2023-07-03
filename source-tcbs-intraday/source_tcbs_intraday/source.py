fe5ec92a70877ff67db1162c7d8beaedfe3c255e
jazzDung <dungpham.020901@gmail.com>
2023-07-03 22:50:27 +0700

    intraday incremental baby!!!

--- source-tcbs-intraday/source_tcbs_intraday/source.py
+++ source-tcbs-intraday/source_tcbs_intraday/source.py
@@ -13,11 +13,7 @@ from airbyte_cdk.sources.streams.http.auth import NoAuth
 
 class Symbol(HttpStream, IncrementalMixin):
     url_base = None
-    primary_key = cursor_field = "tradingDate"
-
-    def str_to_date(self, string):  
-        "'2000-01-01' -> datetime.date(2000, 1, 1)"
-        return datetime.strptime(string, '%Y-%m-%d').date()
+    primary_key = cursor_field = "id"
 
     def reset_cursor_value(self):
 
@@ -36,17 +32,14 @@ class Symbol(HttpStream, IncrementalMixin):
         response = requests.get(self.url).text.split(",")
         response = response[:5] if self.fast_mode else response
         _cursor_value =  dict.fromkeys(response, -1)
-        _cursor_value["date"] = date.today()
-        return _cursor_value
-    
-    def reset_cursor_date(self):
-        return {"date": date.today()}
+        _cursor_date = {"date": date.today()}
+        return _cursor_date | _cursor_value
     
     def get_page_list(self, symbol):
         url = f'https://apipubaws.tcbs.com.vn/stock-insight/v1/intraday/{symbol}/his/paging?page=0&size=1'
         page_amount = requests.get(url).json()["total"]
-        page_num = page_amount//5
-        return [i for i in range (0, page_num)]        
+        page_num = page_amount//self.page_size
+        return [i for i in range (page_num, -1, -1)]        
         
     @property  
     def use_cache(self) -> bool:  
@@ -58,21 +51,20 @@ class Symbol(HttpStream, IncrementalMixin):
 
         self.fast_mode = config["Fast mode"]
         self.url = config["Symbol URL"]
+        self.page_size = config["Page size"]
         self._cursor_value = self.reset_cursor_value()
-        self._cursor_date = self.reset_cursor_date()
 
     @property
     def state(self) -> Mapping[str, Any]:
         "Return the _cursor_value to show on UI at Connection > Settings  > Advanced"
-        # return self._cursor_value
-        return self._cursor_date | self._cursor_value
+        return self._cursor_value
     
     @state.setter
     def state(self, value: Mapping[str, Any]):
         "Update _cursor_value with latest timestamp in ingested record"
-        for key in self._cursor_value:    
-            self._cursor_value[key] = value['id']
-    
+        self._cursor_value = value
+        self._cursor_value["date"] = datetime.strptime(value["date"], '%Y-%m-%d').date()
+
     def next_page_token(self, response: requests.Response):
         "The API does not offer pagination, so we return None to indicate there are no more pages in the response"
         return None
@@ -109,15 +101,16 @@ class SymbolSubStream(HttpSubStream, Symbol, ABC):
     def __init__(self, config: Mapping[str, Any], parent: Symbol, **kwargs):
         super().__init__(config=config, parent=parent, **kwargs)
 
+
 class StockIntraday(SymbolSubStream):
     state_checkpoint_interval = None
 
     def path(self, *, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> str:
         "URL example: 'https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker=TCB&type=stock&resolution=D&from=1687798800&to=1687798800'"
         if datetime.now().weekday() > 4: #today is weekend
-            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/intraday/{stream_slice["symbol"]}/his/paging?page={stream_slice["page"]}&size=5&headIndex=-1'
+            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/intraday/{stream_slice["symbol"]}/his/paging?page={stream_slice["page"]}&size={self.page_size}&headIndex=-1'
         else:
-            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/intraday/{stream_slice["symbol"]}/his/paging?page={stream_slice["page"]}&size=5'
+            return f'https://apipubaws.tcbs.com.vn/stock-insight/v1/intraday/{stream_slice["symbol"]}/his/paging?page={stream_slice["page"]}&size={self.page_size}'
 
     def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
         "Get the symbol list" 
@@ -127,31 +120,47 @@ class StockIntraday(SymbolSubStream):
     
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         response = response.json()
-        base_index = response["total"] - response["page"] * response["size"]
-        ticker = response["ticker"]
+        page = response["page"]
         total = response["total"]
-        response = response["data"]
-        for record in response:
-            record["ticker"] = ticker
-            record["total"] = total
-            record["id"] = base_index-response.index(record) - 1
-            yield record
+        size = self.page_size
+        ticker = response["ticker"]
+
+        if page == total//size:
+            base_index = 0
+        else:
+            base_index = total - size * (page + 1)
+
+        if base_index > self._cursor_value[ticker]:
+            response = response["data"]
+            response.reverse()
+            for record in response:
+                record["ticker"] = ticker
+                record["id"] = base_index + response.index(record)
+                yield record
 
     def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
         "Update symbol's cursor value with highest timestamp in corresponding symbol's record"
-        
+
+        if self._cursor_value["date"] < date.today():
+            self._cursor_value["date"] = date.today()
+            self._cursor_value = self.reset_cursor_value()
+
         for record in super().read_records(*args, **kwargs):            
-            # if self._cursor_value[record["ticker"]] < record["id"]:
-            self._cursor_value[record["ticker"]] = record["id"]
-            yield record
+            if self._cursor_value[record["ticker"]] < record["id"]:
+                self._cursor_value[record["ticker"]] = record["id"]
+                yield record
         
-        # print(f'{record["ticker"]}: {record["id"]}')
-        # print(f'Cursor: {self._cursor_value[record["ticker"]]["records"]}')
-        # print(f'{self._cursor_value}')
- 
 # Source
 class SourceTcbsIntraday(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
+        "Test the connector, check for invalid arguments"
+        try:
+            requests.get(config["Symbol URL"]).text.split(",")
+        except:
+            return False, "Invalid URL or invalid file content format"
+        
+        if config["Page size"] > 100:
+            return False, "Page size must be smaller or equal to 100"
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
